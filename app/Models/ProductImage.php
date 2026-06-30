3988d7a42996a9eda141dc24526800ba6c49e335
AliBaltistani <83259990+AliBaltistani@users.noreply.github.com>
2026-06-30 13:02:15 +0500

    updated product image on create new

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -28,4 +28,9 @@ class ProductImage extends Model
     {
         return $this->belongsTo(Product::class);
     }
+
+    public function setImagePathAttribute($value)
+    {
+        $this->attributes['image_path'] = str_replace('\\', '/', $value);
+    }
 }
